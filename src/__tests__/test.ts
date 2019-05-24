5965471e842812497eed20d7b5fcf11a6608f5f5
unclebean <unclebean2013@gmail.com>
2019-05-24 13:18:34 +0000

    test

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -3,4 +3,8 @@ describe('calculate', function() {
       let result = 5 + 2;
       expect(result).toBe(7);   
   });
+  it('minus', function() {
+      let result = 5 - 3;
+      expect(result).toBe(2);
+  });
 });
